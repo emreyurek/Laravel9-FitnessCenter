06093be55fccddbbd3f7ea84272a5c055b10dd04
emreyurek <yurekemre407@gmail.com>
2022-04-10 19:30:02 +0300

    CategoryController.php and "Add data" form created, insert data to database

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminPanel\CategoryController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminPanel\HomeController as AdminHomeController;
@@ -24,3 +25,12 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 //*******Admin Panel Routes*******
 
 Route::get('/admin', [AdminHomeController::class, 'index'])->name('admin');
+
+//*******Admin Category Routes*******
+
+Route::get('/admin/category',[CategoryController::class,'index'])->name('admin_category');
+Route::get('/admin/category/create',[CategoryController::class,'create'])->name('admin_category_create');
+Route::post('/admin/category/store',[CategoryController::class,'store'])->name('admin_category_store');
+
+
+
